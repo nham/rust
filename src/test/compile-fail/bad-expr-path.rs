63ed4f564c340bae1828cae17a98518bfe65eb4e
Nick Hamann <nick@wabbo.org>
2015-06-05 18:29:56 -0500

    Fix many tests.

--- src/test/compile-fail/bad-expr-path.rs
+++ src/test/compile-fail/bad-expr-path.rs
@@ -1,4 +1,4 @@
-// Copyright 2012 The Rust Project Developers. See the COPYRIGHT
+// Copyright 2012-2015 The Rust Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution and at
 // http://rust-lang.org/COPYRIGHT.
 //
@@ -8,8 +8,8 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-// error-pattern: unresolved name `m1::a`. Did you mean `args`?
-
 mod m1 {}
 
-fn main(args: Vec<String>) { log(debug, m1::a); }
+fn main(args: Vec<String>) {
+    println!("{:?}", m1::a); //~ ERROR unresolved name `m1::a`. Did you mean `args`?
+}
